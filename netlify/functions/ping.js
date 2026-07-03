907b08813a7387b63bbe1fd1f93d59971e8b2d24
koreahan <ehtk113@gmail.com>
2026-07-03 09:25:49 +0900

    Update ping.js

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -1,13 +1,26 @@
+// netlify/functions/ping.js
+
 exports.handler = async () => {
-  const hasEnv = Boolean(
-    process.env.COUPANG_ACCESS_KEY &&
-    process.env.COUPANG_SECRET_KEY &&
-    process.env.COUPANG_SUB_ID
-  );
+  const hasAccessKey = Boolean(process.env.COUPANG_ACCESS_KEY);
+  const hasSecretKey = Boolean(process.env.COUPANG_SECRET_KEY);
+  const hasSubId = Boolean(process.env.COUPANG_SUB_ID);
 
   return {
     statusCode: 200,
-    headers: { "Content-Type": "application/json; charset=utf-8" },
-    body: JSON.stringify({ success: true, hasEnv })
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({
+      success: true,
+      service: "coupangga-deeplink-gate",
+      functionsAlive: true,
+      hasEnv: hasAccessKey && hasSecretKey,
+      env: {
+        accessKey: hasAccessKey,
+        secretKey: hasSecretKey,
+        subId: hasSubId
+      }
+    })
   };
 };
